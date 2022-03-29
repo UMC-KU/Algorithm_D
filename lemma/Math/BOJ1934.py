62c795357d74462503777abb84ea0d94586f8fe9
euna <christinejung10@daum.net>
2022-03-30 07:35:06 +0900

    Add Math - BOJ1934

---
+++ lemma/Math/BOJ1934.py
@@ -0,0 +1,8 @@
+import sys
+from math import lcm
+
+T = int(sys.stdin.readline())
+
+for i in range(T):
+    numList = list(map(int, sys.stdin.readline().split()))
+    print(lcm(numList[0], numList[1]))
