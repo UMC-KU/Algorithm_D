dc3e1075c26c410abe18ee6bed138ce3b130d34b
euna <christinejung10@daum.net>
2022-03-30 01:13:38 +0900

    Add Math - BOJ2745

---
+++ lemma/Math/BOJ2745.py
@@ -0,0 +1,9 @@
+N, B = input().split(" ")  # B진법 수 N
+
+numbers = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+result = 0
+
+for i in range(len(N)):
+    result += (int(B) ** (len(N) - 1 - i)) * int(numbers.index(N[i]))
+
+print(result)
