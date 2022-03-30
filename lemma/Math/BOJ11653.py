e9d038536f2c38505ad2ebe56eaf4f88eee6e53e
euna <christinejung10@daum.net>
2022-03-30 11:58:45 +0900

    Add Math - BOJ11653

---
+++ lemma/Math/BOJ11653.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+if N != 1:
+    i = 2
+    while True:
+        if N % i == 0:
+            print(i)
+            N //= i
+        else:
+            i += 1
+
+        if N == 1:
+            break
