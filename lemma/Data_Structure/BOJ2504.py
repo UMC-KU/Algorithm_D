e9561720daa8ddb1fe91e8f129e449b306b36ff9
euna <christinejung10@daum.net>
2022-05-12 22:37:17 +0900

    Fix Data_Structure - BOJ2504

--- lemma/Data_Structure/BOJ2504.py
+++ lemma/Data_Structure/BOJ2504.py
@@ -8,23 +8,25 @@ line = sys.stdin.readline().strip()
 stack = []  # (), []
 temp = ""  # stack에서 pop한 값
 
-calc_stack = deque()  # 계산해야 하는 값들
+calc_queue = deque()  # 계산해야 하는 값들
 calc = 0  # 계산 결과값 -> stack에 push
 
 result = 0
+
+# 올바르지 않은 괄호열 판독
 result_status = True
 status = False
 
 for l in line:
-    # 열린 괄호 -> push
+    # 여는 괄호 -> push
     if l == "(" or l == "[":
         stack.append(l)
 
-    # 닫힌 괄호 -> 해당 짝까지 pop
+    # 닫는 괄호 -> 해당 짝까지 pop
     elif l == ")":
         while temp != "(" and len(stack) > 0:
             temp = stack.pop()
-            calc_stack.append(temp)
+            calc_queue.append(temp)
             if temp == "(":
                 status = True
 
@@ -37,7 +39,7 @@ for l in line:
     elif l == "]":
         while temp != "[" and len(stack) > 0:
             temp = stack.pop()
-            calc_stack.append(temp)
+            calc_queue.append(temp)
             if temp == "[":
                 status = True
 
@@ -54,27 +56,31 @@ for l in line:
 
     # 닫힌 괄호 -> calc_stack에 들어간 값 계산 & 결과값을 stack에 push
     if l == ")" or l == "]":
-        if len(calc_stack) > 1:  # calc_stack에 값이 2개 이상 들어갔다면 (괄호가 2개 이상 들어갔다면 바로 종료!)
-            temp = calc_stack.popleft()
-            if temp != "(" and temp != "[":  # temp가 숫자일 경우
+        # calc_stack에 값이 2개 이상 들어간 경우 (괄호가 2개 이상 들어갔다면 종료)
+        if len(calc_queue) > 1:
+            temp = calc_queue.popleft()  # 첫번째 값
+
+            # 첫번째 값이 숫자인 경우
+            if temp != "(" and temp != "[":
                 calc += temp
+                temp = calc_queue.popleft()  # 두번째 값
 
-                temp = calc_stack.popleft()
-                if temp != "(" and temp != "[":  # 2번째 pop한 값도 숫자일 경우 -> 숫자끼리 전부 더하고 마지막에 곱하기
+                # 2번째 값도 숫자일 경우 -> 숫자끼리 전부 더하고 마지막에 괄호값 곱하기
+                if temp != "(" and temp != "[":
                     calc += temp
-
-                    while len(calc_stack) > 1:
-                        temp = calc_stack.popleft()
+                    while len(calc_queue) > 1:
+                        temp = calc_queue.popleft()
                         calc += temp
 
-                    temp = calc_stack.popleft()  # 괄호만 남은 상황
+                    temp = calc_queue.popleft()  # 괄호만 남은 상황
                     if temp == "(":
                         calc *= 2
                     else:
                         calc *= 3
 
-                else:  # 2번째 pop한 값이 괄호인 경우 -> 곱하기 (단, calc_stack에 값이 더 남아 있으면 안됨)
-                    if len(calc_stack) == 0:
+                # 1번째 값은 숫자, 2번째 값은 괄호인 경우 -> 곱하기 (단, calc_stack에 값이 더 남아 있다면 종료)
+                else:
+                    if len(calc_queue) == 0:
                         if temp == "(":
                             calc *= 2
                         else:
@@ -83,18 +89,23 @@ for l in line:
                         result_status = False
                         break
 
-            else:  # temp가 괄호일 경우 -> 짤
+            # calc_stack의 길이가 2 이상, 첫번째 값이 괄호인 경우 -> 종료
+            else:
                 result_status = False
                 break
 
+        # calc_stack에 값이 1개만 들어간 경우 ('(' 또는 '[')
         else:
-            temp = calc_stack.popleft()
+            temp = calc_queue.popleft()
             if temp == "(":
                 calc += 2
             else:
                 calc += 3
 
+        # calc_stack 계산 결과값을 stack에 push
         stack.append(calc)
+
+        # 초기화
         calc = 0
         temp = ""
 
