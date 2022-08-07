8e7cda764cd8191d76ff992ff479967369ee23e7
siejwkaodj <54460736+siejwkaodj@users.noreply.github.com>
2022-08-07 23:44:19 +0900

    [edit] 0807

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## 아프로 / 한성준 PS 폴더
-[notion 링크] (https://www.notion.so/Algorithm_D-3b4daef49393400b8f1556d391ef08e6)
+> [notion 링크] (https://www.notion.so/Algorithm_D-3b4daef49393400b8f1556d391ef08e6)   
+ 
+> [코딩테스트 문제집 링크] https://github.com/UMC-KU/Algorithms_Challenge
--- a_pro/09_DP/15486.cpp
+++ a_pro/09_DP/15486.cpp
@@ -1,62 +1,37 @@
+// Baekjoon No. 15486 - 퇴사 2
+// 
 #include <iostream>
-#define ROWS 2
+#include <vector>
 using namespace std;
-void print_arr(int**, int*, int);
 
 int main(){
-    int** arr = new int*[2];
-    
+    ios::sync_with_stdio(false);
+
     int i, j, n, max_profit = 0;
-    cin >> n;
-    int* result = new int[n+1]{0};
-    for(i = 0; i < ROWS; i++){
-        arr[i] = new int[n+1]{0};
-    }
+    scanf("%d", &n);
+    vector<vector<int>> arr(2, vector<int>(n, 0));  // 일정 저장하는 배열
+    vector<int> result(n, 0);   // 결과 저장하는 배열
+
     // 둘째 줄 이하
     for(i = 0; i < n; i++){
-        cin >> arr[0][i] >> arr[1][i];
+        scanf(" %d %d", &arr[0][i], &arr[1][i]);
     }
 
     // DP
     for(i = 0; i < n; i++){
-        if(arr[0][i] + i < n+1 && result[arr[0][i] + i] < arr[1][i] + result[i]){       // 7일차 넘지 않을때까지만
-            result[arr[0][i] + i] = arr[1][i] + result[i];
+        if(arr[0][i] + i <= n && result[arr[0][i] + i - 1] < arr[1][i] + result[i]){       // 7일차 넘지 않을때까지만
+            result[arr[0][i] + i - 1] = arr[1][i] + result[i];
         }
     }    
 
-    print_arr(arr, result, n+1);
     // 최대 수익 선정 부분
     for(i = 0; i < n+1; i++)
         if(result[i] > max_profit)
             max_profit = result[i];
+    // 출력 부분
+    printf("%d\n", max_profit);
+    for(i = 0; i < n; i++)
+        printf("%d ", result[i]);
 
-    cout << max_profit;
-
-
-    for(i = 0; i < ROWS; i++){
-        delete[] arr[i];
-    }
-    delete[] arr;
-    delete[] result;
     return 0;
-}
-
-void print_arr(int** arr, int* result, int n){
-    cout << endl << "arr 출력\n";/
-    for(int i = 0; i < n; i++)
-        printf("%3d ", i);
-    cout << endl;
-    for(int i = 0; i < ROWS; i++){
-        for(int j = 0; j < n; j++){
-            printf("%3d ", arr[i][j]);
-        }
-        cout << endl;
-    }
-    cout << endl << "result 출력\n";
-
-    for(int i = 0; i < n; i++){
-        printf("%3d ", result[i]);
-    }
-    cout << endl;
-    return;
 }
